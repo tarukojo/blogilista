3d841ad14f6de00686ca5691e669ebbac00c795c
Taru Kojo <tarukojo@gmail.com>
2018-02-10 11:12:52 +0200

    Osa 4, tehtava 4.4. total likes tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    var totalAmount = 0;
+    const totals = blogs.map(function(blog) { 
+        totalAmount = blog.likes + totalAmount 
+    })
+    return totalAmount
+}
   
 module.exports = {
-    dummy
+    dummy, 
+    totalLikes
 }
