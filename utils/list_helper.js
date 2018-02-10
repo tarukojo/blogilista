2587df20d1a8fc3a9cf684071a94bc820c660ee2
Taru Kojo <tarukojo@gmail.com>
2018-02-10 12:35:20 +0200

    Osa 4, tehtava 4.6 mostBlogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,8 +9,44 @@ const totalLikes = (blogs) => {
     })
     return totalAmount
 }
+
+const mostBlogs = (blogs) => {
+
+    const writers = blogs.map(function(blog) {
+        return blog.author
+    })
+
+    var results = []
+
+    writers.map(function(writer) {
+
+        var writerResults = results.filter(function(result) {
+            if (result.author === writer) {
+                result.blogs = result.blogs + 1
+                return result
+            }
+        }) 
+        if (writerResults.length === 0) {
+            results.push({ author: writer, blogs: 1})
+        } 
+    })
+
+    finalResult = { author: "", blogs: 0 }
+
+    results.map(function(result) {
+        if (result.blogs > finalResult.blogs) {
+            finalResult = {
+                author: result.author,
+                blogs: result.blogs
+            }
+        } 
+    })
+
+    return finalResult
+}   
   
 module.exports = {
     dummy, 
-    totalLikes
+    totalLikes,
+    mostBlogs
 }
